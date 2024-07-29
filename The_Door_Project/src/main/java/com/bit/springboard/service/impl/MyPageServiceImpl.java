e54f4f073f361fd0e7b98df2baedaaa134f9cab0
dydhyun <dydgus625@naver.com>
2024-07-29 14:09:20 +0900

    tmp

---
+++ The_Door_Project/src/main/java/com/bit/springboard/service/impl/MyPageServiceImpl.java
@@ -0,0 +1,26 @@
+package com.bit.springboard.service.impl;
+
+import com.bit.springboard.dao.MyPageDao;
+import com.bit.springboard.dto.MemberDto;
+import com.bit.springboard.service.MyPageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyPageServiceImpl implements MyPageService {
+    private MyPageDao myPageDao;
+
+    @Autowired
+    public MyPageServiceImpl(MyPageDao myPageDao) { this.myPageDao = myPageDao;}
+
+    @Override
+    public MemberDto getInfo(MemberDto memberDto) {
+        System.out.println(myPageDao.myInfo("실험용"));
+        return myPageDao.myInfo(memberDto.getNickname());
+    }
+
+    @Override
+    public MemberDto altInfo(MemberDto memberDto) {
+        return null;
+    }
+}
